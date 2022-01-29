aea7e1a0f5d0d8271406b7309236988a68e627ce
Idir Naitali <idir.naitali@vo2-consultants.com>
2022-01-29 12:30:18 +0100

    Init

---
+++ src/test/java/com/afadli/springrestapi/SpringRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afadli.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
