b4fd2e334f7d52cf199b23c9392ebae451b91e50
Assia FADLI <assiafadli.pro@gmail.com>
2022-01-30 02:08:55 +0100

    [FEATURE] Add create task endpoint

--- src/main/java/com/afadli/springrestapi/endpoint/TaskController.java
+++ src/main/java/com/afadli/springrestapi/endpoint/TaskController.java
@@ -3,26 +3,35 @@ package com.afadli.springrestapi.endpoint;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.afadli.springrestapi.model.Task;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @ControllerAdvice
 @RestController
 @RequestMapping(TaskController.PATH)
 public class TaskController {
     public static final String PATH = "/api/v1/tasks";
+    private static Long counter = 4L;
 
-    public static List<Task> tasks = List.of(
-            new Task(1L, "todo 1", "desc 1", LocalDateTime.of(2022, 1, 1, 10, 0)),
-            new Task(2L, "todo 2", "desc 2", LocalDateTime.of(2022, 1, 2, 10, 0)),
-            new Task(3L, "todo 3", "desc 3", LocalDateTime.of(2022, 1, 3, 10, 0))
-    );
+    public static List<Task> tasks = new ArrayList<>(getTasks());
+
+    private static List<Task> getTasks() {
+        return List.of(
+                new Task(1L, "todo 1", "desc 1", LocalDateTime.of(2022, 1, 1, 10, 0)),
+                new Task(2L, "todo 2", "desc 2", LocalDateTime.of(2022, 1, 2, 10, 0)),
+                new Task(3L, "todo 3", "desc 3", LocalDateTime.of(2022, 1, 3, 10, 0))
+        );
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getTask(@PathVariable("id") Long id) {
@@ -31,6 +40,17 @@ public class TaskController {
                 .findFirst()
                 .map(task -> ResponseEntity.ok(task))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTask(@RequestBody Task task){
+        task.setId(counter);
+        tasks.add(task);
+        var createdUri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(Map.of("id", counter++))
+                .toUri();
+        return ResponseEntity.created(createdUri).build();
     }
 
 
--- src/test/java/com/afadli/springrestapi/endpoint/TaskControllerIT.java
+++ src/test/java/com/afadli/springrestapi/endpoint/TaskControllerIT.java
@@ -2,11 +2,13 @@ package com.afadli.springrestapi.endpoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,20 +35,43 @@ public class TaskControllerIT {
                 .build();
     }
 
-    @Test
-    void should_return_404_on_not_existing_task_id() throws Exception {
-        mvc
-                .perform(get(TaskController.PATH + "/{id}", 99))
-                .andExpect(status().isNotFound());
+    @Nested
+    class GetTaskTests{
+
+        @Test
+        void should_return_404_on_not_existing_task_id() throws Exception {
+            mvc
+                    .perform(get(TaskController.PATH + "/{id}", 99))
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        void should_get_task_by_id() throws Exception {
+            mvc
+                    .perform(get(TaskController.PATH + "/{id}", 2))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value("todo 2"))
+                    .andExpect(jsonPath("$.description").value("desc 2"))
+                    .andExpect(jsonPath("$.dateTime").value("2022-01-02T10:00:00"));
+        }
     }
 
-    @Test
-    void should_get_task_by_id() throws Exception {
-        mvc
-                .perform(get(TaskController.PATH + "/{id}", 2))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("todo 2"))
-                .andExpect(jsonPath("$.description").value("desc 2"))
-                .andExpect(jsonPath("$.dateTime").value("2022-01-02T10:00:00"));
+    @Nested
+    class CreateTaskTests{
+
+        @Test
+        void should_create_task() throws Exception {
+            var body = objectMapper.createObjectNode()
+                    .put("name","todo")
+                    .put("description","desc")
+                    .put("dateTime","2023-01-02T10:00:00")
+                    .toString();
+            mvc
+                    .perform(post(TaskController.PATH)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(body))
+                    .andExpect(status().isCreated())
+            .andExpect(header().stringValues("location", "http://localhost/api/v1/tasks/4"));
+        }
     }
 }
